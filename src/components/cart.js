d31426e2db5bda39743f10cc4b864ba6a2ffdca0
Jabed Bhuiyan <jabed.bhoiyan@gmail.com>
2018-04-01 14:05:25 +0600

    Initial

---
+++ src/components/cart.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ImageLoading from './imageloading';
+class Cart extends Component {
+
+
+    render (){
+        var data = "";
+        if(this.props.state.items.length > 0){
+            var index = 0;
+           data = this.props.state.items.map(r => (
+               <div className="item" key={r._id}>
+               <div className="itemimg"><ImageLoading imageUrl={r._source.images[0]}  /></div>
+               <div className="itemname">{r._source.title}</div>
+               <div className="itemprice">৳{r._source.price}</div>
+                <div className="remove" onClick={this.props.removeitem.bind(this,index)} key={index++}><i className="fa fa-times"></i></div>
+               </div>
+                ))
+            
+        }
+        else {
+         data =  <div className="no-item">No item in your cart</div>  
+        }
+        return data;
+        
+    }
+    
+    
+}
+
+export default Cart;
---
+++ src/components/imageloading.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class ImageLoading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+        ctatus: "loading",
+        imageStatus: <img className="laoding" src="img/loading.gif" /> };
+  }
+
+  handleImageLoaded() {
+    this.setState({ ctatus: "loaded", imageStatus: "" });
+  }
+
+  handleImageErrored() {
+    this.setState({  ctatus: "loading",imageStatus: <img src="img/failed.jpg" />  });
+  }
+
+  render() {
+    return (
+      <div className="product-image" >
+        <img id={this.state.ctatus}
+          src={this.props.imageUrl}
+          onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)}
+        />
+        {this.state.imageStatus}
+      </div>
+    );
+  }
+}
+export default ImageLoading;
